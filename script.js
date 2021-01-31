4c88a653e16c093ca1eff9f23f2187906585b045
Tsubasa Goya <tsubasagoya@Tsubasas-MacBook-Pro.local>
2021-01-31 06:05:46 -0800

    initial complete project

---
+++ script.js
@@ -0,0 +1,63 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+let passwordsMatch = false;
+
+function validateForm() {
+    // Using Constraint API
+    isValid = form.checkValidity();
+    if (!isValid) {
+        message.textContent = 'Please fill out all fields.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        return;
+    }
+    // Check to see if passwords match
+    if (password1El.value === password2El.value) {
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+    } else {
+        passwordsMatch = false;
+        message.textContent = 'Make sure passwords match.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+        return;
+    }
+    // If form is valid and passwords match
+    if (isValid && passwordsMatch) {
+        message.textContent = 'Successfuly Registered!';
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+        return true;
+    }
+    return false;
+}
+
+function storeFormData() {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    };
+    // Do something with the data
+    console.log(user);
+}
+
+function processFormData(e) {
+    e.preventDefault();
+    if (validateForm()) {
+        storeFormData();
+    }
+}
+
+// Event Listener
+form.addEventListener('submit', processFormData);
